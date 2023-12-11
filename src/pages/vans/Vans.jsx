a7ba10b75f04ff31a258c524efa5b1b97e0826d2
Mehran Hamzei <mehran.magicianDeveloper@gmail.com>
2023-12-11 18:55:36 +0330

    install npm

--- src/pages/vans/Vans.jsx
+++ src/pages/vans/Vans.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import styles from "./Vans.module.css";
+import axios from "axios";
 
 const Vans = () => {
+  const fetchingVans = async () => {
+    const resp = axios.get("/api/vans");
+  };
+
   return (
     <div>
       <h1 className={styles.title}>Vans Page</h1>
